17a2f452468a625b4c196f69bf37468e840b35d7
Esan Samuel <esansamuel555@gmail.com>
2025-11-29 16:21:59 +0100

    moved bullmq redis from local to upstash

--- src/utils/bullmq-redis.ts
+++ src/utils/bullmq-redis.ts
@@ -1,8 +1,18 @@
 import IORedis from "ioredis";
+import logger from "./logger";
 
-export const bullRedis = new IORedis({
-  host: "127.0.0.1",
-  port: 6379,
-  maxRetriesPerRequest: null, 
-  enableOfflineQueue: true,
+export const bullRedis = new IORedis(
+  process.env.UPSTASH_REDIS_REST_IOREDIS_TOKEN!,
+  {
+    maxRetriesPerRequest: null,
+    enableOfflineQueue: true,
+  }
+);
+
+bullRedis.on("connect", () => {
+  logger.info("✅ BullRedis upstash connected successfully");
+});
+
+bullRedis.on("error", (err) => {
+  logger.error({ err }, "❌ Redis connection failed");
 });
